5d156c95cd978a00c8276324981c9bdbdc575c14
Rehman Ul Haq <workmythical@gmail.com>
2026-02-15 02:43:33 -0800

    Configure for both Vercel and GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/M-A-Carpets/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/M-A-Carpets/' : '/',
   server: {
     port: 3000,
     open: true
